03faccddc001bc24f7a6e248296e62b24234e4c5
IgorDroma <igordroma@gmail.com>
2025-11-12 15:46:37 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <!-- Підключення Tailwind через Play CDN -->
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
       </Head>
       <Component {...pageProps} />
